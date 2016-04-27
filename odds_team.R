0363887b62f7b5cb373b2331e75175fbef5a3e68
mdlee12 <mdlee12@gmail.com>
2016-04-27 02:00:22 -0400

    modified code to update DEN hex and margin for caption

--- odds_team.R
+++ odds_team.R
@@ -10,7 +10,7 @@ colnames(odds) <- c("Team","Pick","Probability")
 odds$Pick <- as.numeric(odds$Pick)
 odds$Team <- as.character(odds$Team)
 odds$Selecting <- ifelse(odds$Team=="LAL" & odds$Pick > 3,"PHI",ifelse(odds$Team=="SAC" & odds$Pick > 10,"CHI",ifelse(odds$Team=="WAS" & odds$Pick > 3,"PHX",odds$Team)))
-odds$Selecting <- factor(odds$Selecting, levels=c("PHI", "PHX",  "BOS", "LAL", "MIN", "NOP", "DEN","MIL", "SAC", "TOR",  "ORL", "UTA", "CHI","WAS"))
+odds$Selecting <- factor(odds$Selecting, levels=c("PHI", "PHX",  "BOS", "LAL", "MIN", "NOP", "DEN","MIL", "SAC", "TOR", "ORL", "UTA", "CHI","WAS"))
 odds$Pick <- factor(odds$Pick, levels = c(14:1))
 odds$Probability <- ifelse(is.na(odds$Probability),0,odds$Probability)
 
@@ -21,13 +21,13 @@ g <- g + scale_y_continuous(limits=c(0, 1), expand = c(0, 0),breaks = seq(0, 1, 
          subtitle = "Alternative, pick oriented view. After tiebreakers, pick trades, and swaps. Based on 100,000 simulations.",
          caption="Reproduced by: @mikeleeco                  Original: @dsparks                  Source: http://www.nba.com/celtics/news/sidebar/2016-draft-lottery-qa") +
     coord_flip() +
-    scale_fill_manual(name="Pick",values = c("#006BB6", "#E56020","#008348", "#FDB927", "#C4CED3","#B4975A", "#4D90CD","#F0EBD2", "#724C9F", "#061922","#007DC5", "#00471B", "#CE1141", "#002B5C"))
+    scale_fill_manual(name="Pick",values = c("#006BB6", "#E56020","#008348", "#FDB927", "#C4CED3","#B4975A", "#4FA8FF","#F0EBD2", "#724C9F", "#061922","#007DC5", "#00471B", "#CE1141", "#002B5C"))
 g  + theme(
   axis.text.x = element_text(size=14,margin=margin(b=5),color = "black"),
   axis.title.x = element_text(size=16),
   plot.title = element_text(size=30),
   plot.subtitle = element_text(size=14),
-  plot.caption = element_text(size=16,face = "italic"),
+  plot.caption = element_text(size=16,face = "italic",hjust=.5,margin=margin(t=5)),
   axis.text.y = element_text(size=18,colour = "black"),
   axis.ticks.y=element_blank(),
   axis.ticks.x=element_blank(),
